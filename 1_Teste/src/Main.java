a2df5daeee7bcd5bd7db33b1675b9d98d1d692a2
Leandro Alves <leandro_alvesbr@outlook.com>
2024-03-15 16:13:35 -0300

    aulas poo java univesp

---
+++ 1_Teste/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println ("Only a test!");
+		
+		String test = "test 1";
+		String Test = "Test 2";
+		String TEST = "TEST 3";
+		
+		System.out.println(test);
+		System.out.println(Test);
+		System.out.println(TEST);
+		
+	}
+}
